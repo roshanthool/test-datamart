0eeb9a8f798fea54897904b7d556d0efc50eb50e
priya <awatepriya@gmail.com>
2022-10-16 10:34:00 +0530

    initial commit

---
+++ com/test/source_data_loading.py
@@ -0,0 +1,28 @@
+from pyspark.sql import SparkSession
+import yaml
+import os.path
+import utils.aws_utils as ut
+
+if __name__ == '__main__':
+
+    os.environ["PYSPARK_SUBMIT_ARGS"] = (
+        '--packages "mysql:mysql-connector-java:8.0.15" pyspark-shell'
+    )
+
+    # Create the SparkSession
+    spark = SparkSession \
+        .builder \
+        .appName("Read ingestion enterprise applications") \
+        .master('local[*]') \
+        .getOrCreate()
+    spark.sparkContext.setLogLevel('ERROR')
+
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    app_config_path = os.path.abspath(current_dir + "/../../../../" + "application.yml")
+    app_secrets_path = os.path.abspath(current_dir + "/../../../../" + ".secrets")
+
+    conf = open(app_config_path)
+    app_conf = yaml.load(conf, Loader=yaml.FullLoader)
+    secret = open(app_secrets_path)
+    app_secret = yaml.load(secret, Loader=yaml.FullLoader)
+
