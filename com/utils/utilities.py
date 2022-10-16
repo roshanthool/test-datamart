5a092e8458641ff4c8cabfdc98b8d7dbfd45e4c6
anusha <anushasurapaneni08@gmail.com>
2022-10-16 08:34:34 +0100

    Initial Commit

--- com/utils/utilities.py
+++ com/utils/utilities.py
@@ -1,6 +1,6 @@
 import os.path
 def get_mysql_jdbc_url(mysql_config: dict):
-    print("In my sql func")
+    print("mysql_config")
     host = mysql_config["mysql_conf"]["hostname"]
     port = mysql_config["mysql_conf"]["port"]
     database = mysql_config["mysql_conf"]["database"]
