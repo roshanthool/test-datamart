c25d79a7d108dee9649a82f5940f1ecc1eb9c01a
anusha <anushasurapaneni08@gmail.com>
2022-10-17 23:03:20 +0100

    Mongo Commit

--- com/test/source_data_loading.py
+++ com/test/source_data_loading.py
@@ -11,6 +11,15 @@ if __name__ == '__main__':
         '--packages "mysql:mysql-connector-java:8.0.15" pyspark-shell'
     )
 
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    app_config_path = os.path.abspath(current_dir + "/../../" + "application.yml")
+    app_secrets_path = os.path.abspath(current_dir + "/../../" + ".secrets")
+
+    conf = open(app_config_path)
+    app_conf = yaml.load(conf, Loader=yaml.FullLoader)
+    secret = open(app_secrets_path)
+    app_secret = yaml.load(secret, Loader=yaml.FullLoader)
+
     # Create the SparkSession
     spark = SparkSession \
         .builder \
@@ -20,15 +29,6 @@ if __name__ == '__main__':
         .config("spark.mongodb.input.uri", app_secret["mongodb_config"]["uri"]) \
         .getOrCreate()
     spark.sparkContext.setLogLevel('ERROR')
-
-    current_dir = os.path.abspath(os.path.dirname(__file__))
-    app_config_path = os.path.abspath(current_dir + "/../../" + "application.yml")
-    app_secrets_path = os.path.abspath(current_dir + "/../../" + ".secrets")
-
-    conf = open(app_config_path)
-    app_conf = yaml.load(conf, Loader=yaml.FullLoader)
-    secret = open(app_secrets_path)
-    app_secret = yaml.load(secret, Loader=yaml.FullLoader)
 
     src_list = app_conf['source_list']
     staging_loc = app_conf['staging_loc']
