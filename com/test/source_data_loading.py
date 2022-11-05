f6348c40706654a0db324007479bc51084d0b167
anusha <anushasurapaneni08@gmail.com>
2022-11-05 08:04:31 +0000

    Load Commit

--- com/test/source_data_loading.py
+++ com/test/source_data_loading.py
@@ -80,11 +80,11 @@ if __name__ == '__main__':
                 .add("REGIS_CHANNEL", StringType(), True) \
                 .add("REGIS_GENDER", StringType(), True) \
                 .add("REGIS_CITY", StringType(), True) \
-                .add("CHILD_ID", StringType(), True)
-                #.add("CHILD_NB", IntegerType(), True) \
-                #.add("CHILD_GENDER", StringType(), True) \
-                #.add("CHILD_DOB", StringType(), True) \
-                #.add("CHILD_DECEASED", StringType(), True)
+                .add("CHILD_ID", StringType(), True) \
+                .add("CHILD_NB", IntegerType(), True) \
+                .add("CHILD_GENDER", StringType(), True) \
+                .add("CHILD_DOB", StringType(), True) \
+                .add("CHILD_DECEASED", StringType(), True)
             cp_df = ut.read_from_s3(src_conf,fin_schema,spark)
             cp_df = cp_df.withColumn('ins_dt', current_date())
             cp_df.show()
